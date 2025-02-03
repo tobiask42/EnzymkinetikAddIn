68b3d56a7ae412765af7d39b77fa75cecbdddc6c
tobias <tobiaskoch93@yahoo.de>
2025-02-03 18:51:07 +0100

    refactored form factory

--- EnzymkinetikAddIn/Factories/FormFactory.cs
+++ EnzymkinetikAddIn/Factories/FormFactory.cs
@@ -10,55 +10,17 @@ namespace EnzymkinetikAddIn.Factories
 {
     public class FormFactory
     {
-        private readonly Dictionary<string, IFormGenerator> _generators = new Dictionary<string, IFormGenerator>();
-        private readonly Dictionary<string, IModelLogic> _logics = new Dictionary<string, IModelLogic>();
+        private readonly DataSetGenerator _dataSetGenerator;
 
         public FormFactory()
         {
-            //foreach (var model in ModelConstants.Models)
-            //{
-            //    RegisterModel(model.Key, model.Value.GeneratorClass, model.Value.LogicClass);
-            //}
+            _dataSetGenerator = new DataSetGenerator();
         }
 
-
-        //private void RegisterModel(string modelName, string generatorClassName, string logicClassName)
-        //{
-        //    var generatorType = Type.GetType(generatorClassName);
-        //    var logicType = Type.GetType(logicClassName);
-
-        //    if (generatorType == null || logicType == null)
-        //    {
-        //        throw new InvalidOperationException($"Typen für Modell '{modelName}' nicht gefunden. Generator: {generatorClassName}, Logik: {logicClassName}");
-        //    }
-
-        //    var generator = Activator.CreateInstance(generatorType) as IFormGenerator;
-        //    var logic = Activator.CreateInstance(logicType) as IModelLogic;
-
-        //    if (generator == null || logic == null)
-        //    {
-        //        throw new InvalidOperationException($"Fehler beim Erstellen von Instanzen für '{modelName}'.");
-        //    }
-
-        //    _generators[modelName] = generator;
-        //    _logics[modelName] = logic;
-        //}
-
-
-
-
-        public BaseModelForm CreateForm(string modelType)
+        public BaseForm CreateForm(string concentration, string unit)
         {
-            if (_generators.TryGetValue(modelType, out var generator))
-            {
-                return generator.GenerateForm();
-            }
-            throw new ArgumentException($"Unbekannter Modelltyp: {modelType}");
+            return _dataSetGenerator.GenerateForm(concentration, unit);
         }
 
-        public IEnumerable<string> GetAvailableModels()
-        {
-            return _generators.Keys.Where(key => key != "Kalibriergerade");
-        }
     }
 }
