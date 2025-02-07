6f95f7d439fc9d95103a56332776b632cebcb815
tobias <tobiaskoch93@yahoo.de>
2025-02-07 18:43:35 +0100

    added functionality for adding data to table

--- EnzymkinetikAddIn/Generators/EditFormGenerator.cs
+++ EnzymkinetikAddIn/Generators/EditFormGenerator.cs
@@ -14,70 +14,86 @@ namespace EnzymkinetikAddIn.Generators
     internal class EditFormGenerator
     {
         public BaseForm GenerateForm(string tableName)
+        {
+            // Lade die Daten aus der Datenbank
+            DataTable tableData = DatabaseHelper.LoadTable(tableName);
+
+            // Erstelle das Formular
+            BaseForm form = new BaseForm();
+
+            // Zugriff auf das DataGridView des Formulars
+            var dataGridView = form.GetDataGridView();
+
+            // Initialisiere die Spalten über den ColumnManager
+            ColumnManager columnManager = new ColumnManager(dataGridView);
+
+            List<DataColumn> commentColumns = new List<DataColumn>(); // Speichert Kommentar-Spalten zur späteren Verarbeitung
+
+            string currentTimeUnit = "";
+            // Über die Spalten des DataTables iterieren
+            foreach (DataColumn column in tableData.Columns)
+            {
+                string columnLabel = column.ColumnName;
+                if (columnLabel.Contains("Zeit"))
                 {
-                    // Lade die Daten aus der Datenbank
-                    DataTable tableData = DatabaseHelper.LoadTable(tableName);
-
-                    // Erstelle das Formular
-                    BaseForm form = new BaseForm();
-
-                    // Zugriff auf das DataGridView des Formulars
-                    var dataGridView = form.GetDataGridView();
-
-                    // Initialisiere die Spalten über den ColumnManager
-                    ColumnManager columnManager = new ColumnManager(dataGridView);
-
-                    // Über die Spalten des DataTables iterieren
-                    foreach (DataColumn column in tableData.Columns)
+                    string timeunit = Char.ToString(columnLabel.ElementAt(6));
+                    if (timeunit.Equals("m"))
                     {
-                        string columnLabel = column.ColumnName;
-                        if (columnLabel.Contains("Zeit"))
-                        {
-                            string timeunit = Char.ToString(columnLabel.ElementAt(6));
-                            columnManager.SetTimeUnit(timeunit);
-                            columnManager.InitializeTimeColumn();
-                        }
-                        else if (columnLabel.Contains("Verdünnung"))
-                        {
-                            string number = Char.ToString(columnLabel.ElementAt(2));
-                            columnManager.InitializeIntCol("dilution_" + number, "c_" + number + "\nVerdünnung");
-
-                        }
-                        else if (columnLabel.Contains("Messwert"))
-                        {
-                            string sampleunit = "";
-                            if (columnLabel.Contains("g_L"))
-                            {
-                                sampleunit = "g/L";
-                            }
-                            else
-                            {
-                                sampleunit = "mg/dL";
-                            }
-                            char c = columnLabel.ElementAt(2);
-                            char num = columnLabel.ElementAt(13);
-                            if (num == '1')
-                            {
-                                columnManager.InitializeDoubleCol("reading_1_" + c, "c_" + c + "\nMesswert 1\n" + sampleunit);
-                            }
-                            else if (num == '2')
-                            {
-                                columnManager.InitializeDoubleCol("reading_2_" + c, "c_" + c + "\nMesswert 2\n" + sampleunit);
-                            }
-                            else
-                            {
-                                MessageBox.Show("Fehler beim Initialisieren der Messwert-Spalte", "Fehler", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                            }
-                        }
-                        else
-                        {
-                            columnManager.InitializeBaseCol("comment", "Kommentar");
-                        }
+                        timeunit = "min";
                     }
-
-
-                    return form;
+                    currentTimeUnit = timeunit;
+                    columnManager.SetTimeUnit(timeunit);
+                    columnManager.InitializeTimeColumn();
+                    WriteToGrid(column, tableData, dataGridView, "time");
                 }
-    
+                else if (columnLabel.Contains("Verdünnung"))
+                {
+                    string number = Char.ToString(columnLabel.ElementAt(2));
+                    string dataGridColumnName = "dilution_" + number;
+                    columnManager.InitializeIntCol(dataGridColumnName, "c_" + number + "\nVerdünnung");
+                    WriteToGrid(column, tableData, dataGridView, dataGridColumnName);
+                }
+                else if (columnLabel.Contains("Messwert"))
+                {
+                    string sampleunit = columnLabel.Contains("g_L") ? "g/L" : "mg/dL";
+                    char c = columnLabel.ElementAt(2);
+                    char num = columnLabel.ElementAt(13);
+                    string dataGridColumnName = num == '1' ? $"reading_1_{c}" : $"reading_2_{c}";
+                    string dataGridColumnLabel = $"c_{c}\nMesswert {num}\n{sampleunit}";
+
+                    columnManager.InitializeDoubleCol(dataGridColumnName, dataGridColumnLabel);
+                    WriteToGrid(column, tableData, dataGridView, dataGridColumnName);
+                }
+                else
+                {
+                    commentColumns.Add(column); // Kommentar-Spalten zwischenspeichern
+                }
+            }
+
+            // Kommentar-Spalte zuletzt hinzufügen
+            columnManager.InitializeBaseCol("comment", "Kommentar");
+            foreach (var column in commentColumns)
+            {
+                WriteToGrid(column, tableData, dataGridView, "comment");
+            }
+            form.SetCurrentTimeUnit(currentTimeUnit);
+            return form;
+        }
+
+
+        private void WriteToGrid(DataColumn column, DataTable tableData, DataGridView dataGridView, string dataGridColumnName)
+        {
+            // Stelle sicher, dass das DataGridView genügend Zeilen hat und füge weitere Eingabezeile hinzu
+            while (dataGridView.Rows.Count <= tableData.Rows.Count)
+            {
+                dataGridView.Rows.Add();
+            }
+
+            // Schreibe die Werte aus der DataTable in die entsprechende Spalte der DataGridView
+            for (int i = 0; i < tableData.Rows.Count; i++)
+            {
+                dataGridView.Rows[i].Cells[dataGridColumnName].Value = tableData.Rows[i][column];
             }
         }
+    }
+}
