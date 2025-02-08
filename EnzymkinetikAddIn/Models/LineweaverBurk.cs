139141875ea6c16d08d92b92e03b7e43dd2cd684
tobias <tobiaskoch93@yahoo.de>
2025-02-08 17:47:50 +0100

    model stubs (to be implemented)

---
+++ EnzymkinetikAddIn/Models/LineweaverBurk.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnzymkinetikAddIn.Interfaces;
+
+namespace EnzymkinetikAddIn.Models
+{
+    internal class LineweaverBurk : IModelLogic
+    {
+        public List<List<DataTable>> CalculateResult(string tableName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
