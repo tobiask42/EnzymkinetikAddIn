03f45dca58bfb8eaebb755d84bb6249c3fabe1f9
tobias <tobiaskoch93@yahoo.de>
2025-02-08 17:47:18 +0100

    model for returning the data put into the table

---
+++ EnzymkinetikAddIn/Models/RawData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnzymkinetikAddIn.Data;
+using EnzymkinetikAddIn.Interfaces;
+using EnzymkinetikAddIn.Utilities;
+
+namespace EnzymkinetikAddIn.Models
+{
+    internal class RawData : IModelLogic
+    {
+        public List<List<DataTable>> CalculateResult(string tableName)
+        {
+            DataTable rawData = DataTransformer.TransformFromDatabase(tableName);
+            //rawData = DatabaseHelper.LoadTable(tableName);
+            return new List<List<DataTable>>
+            {
+                new List<DataTable> { rawData }
+            };
+        }
+    }
+}
