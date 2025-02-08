990223898596b3892f8eb8bb73c347bf2501df5e
tobias <tobiaskoch93@yahoo.de>
2025-02-08 16:26:19 +0100

    Class for transforming Database tables into tables for excel and further processing

---
+++ EnzymkinetikAddIn/Utilities/DataTransformer.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnzymkinetikAddIn.Data;
+
+namespace EnzymkinetikAddIn.Utilities
+{
+    internal class DataTransformer
+    {
+        public static DataTable TransformFromDatabase(string tableName)
+        {
+            DataTable oldTableData = DatabaseHelper.LoadTable(tableName);
+            DataTable newTableData = new DataTable();
+
+            Dictionary<string, string> columnMappings = new Dictionary<string, string>(); // Alt -> Neu Zuordnung
+            string timeUnit = "";
+
+            // Neue Spalten basierend auf den alten Spalten definieren
+            foreach (DataColumn column in oldTableData.Columns)
+            {
+                string columnName = column.ColumnName;
+
+                if (columnName.Contains("Zeit"))
+                {
+                    timeUnit = Char.ToString(columnName.ElementAt(6)) == "m" ? "min" : "s";
+                    string newColumnName = "Zeit (" + timeUnit + ")";
+                    newTableData.Columns.Add(newColumnName, typeof(double));
+                    columnMappings[columnName] = newColumnName; // Alt -> Neu
+                }
+                else if (columnName.Contains("Verdünnung"))
+                {
+                    string number = Char.ToString(columnName.ElementAt(2));
+                    string newColumnName = "Verdünnung " + number;
+                    newTableData.Columns.Add(newColumnName, typeof(double));
+                    columnMappings[columnName] = newColumnName;
+                }
+                else if (columnName.Contains("Messwert"))
+                {
+                    char c = columnName.ElementAt(2);
+                    char num = columnName.ElementAt(13);
+                    string sampleUnit = columnName.Contains("g_L") ? "g/L" : "mg/dL";
+                    string newColumnName = num == '1' ? $"Messwert 1 (c_{c}, {sampleUnit})" : $"Messwert 2 (c_{c}, {sampleUnit})";
+                    newTableData.Columns.Add(newColumnName, typeof(double));
+                    columnMappings[columnName] = newColumnName;
+                }
+                else
+                {
+                    string newColumnName = "Kommentar";
+                    newTableData.Columns.Add(newColumnName, typeof(string));
+                    columnMappings[columnName] = newColumnName;
+                }
+            }
+            // Daten spaltenweise übertragen
+            int rowCount = oldTableData.Rows.Count;
+            for (int i = 0; i < rowCount; i++)
+            {
+                DataRow newRow = newTableData.NewRow();
+
+                foreach (var mapping in columnMappings)
+                {
+                    string oldColumn = mapping.Key;
+                    string newColumn = mapping.Value;
+
+                    newRow[newColumn] = oldTableData.Rows[i][oldColumn]; // Werte direkt übertragen
+                }
+
+                newTableData.Rows.Add(newRow);
+            }
+
+            return newTableData;
+        }
+
+    }
+}
