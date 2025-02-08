4aeb8520360b64609eb5ffe4bbd7cee79e43b98b
tobias <tobiaskoch93@yahoo.de>
2025-02-08 17:45:52 +0100

    factory for returning models

---
+++ EnzymkinetikAddIn/Factories/ModelFactory.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EnzymkinetikAddIn.Interfaces;
+using EnzymkinetikAddIn.Models;
+
+namespace EnzymkinetikAddIn.Factories
+{
+    internal class ModelFactory
+    {
+
+        public IModelLogic GenerateModel(string modelName)
+        {
+            switch (modelName)
+            {
+                case "Rohdaten":
+                    return new RawData();
+                case "Michaelis-Menten":
+                    return new MichaelisMenten();
+                case "Lineweaver-Burk":
+                    return new LineweaverBurk();
+                default:
+                    throw new ArgumentException($"Unbekanntes Modell: {modelName}");
+            }
+        }
+    }
+}
