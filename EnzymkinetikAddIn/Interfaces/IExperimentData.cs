b4105b6bba55d256e926e18bc5552da2e0fc1664
tobias <tobiaskoch93@yahoo.de>
2025-02-08 15:47:14 +0100

    interfaces for data transfer

--- EnzymkinetikAddIn/Interfaces/IExperimentData.cs
+++ EnzymkinetikAddIn/Interfaces/IExperimentData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,7 @@ namespace EnzymkinetikAddIn.Interfaces
 {
     internal interface IExperimentData
     {
-        // Methode zur Validierung der Daten
-        bool Validate();
-
+        string ExperimentName { get; }
+        DataTable GetRawData();
     }
 }
--- EnzymkinetikAddIn/Interfaces/IModelLogic.cs
+++ EnzymkinetikAddIn/Interfaces/IModelLogic.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,5 +10,7 @@ namespace EnzymkinetikAddIn.Interfaces
 {
     internal interface IModelLogic
     {
+        string ModelName { get; }
+        DataTable CalculateModel(DataTable rawData); // Führt Berechnungen mit den Rohdaten durch und gibt die Ergebnisse zurück
     }
 }
