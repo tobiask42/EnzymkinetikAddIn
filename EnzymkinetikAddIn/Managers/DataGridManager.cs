d47cba88833aa351b7e7535ade6f7aa3a7530d9d
tobias <tobiaskoch93@yahoo.de>
2025-02-20 20:04:55 +0100

    adjusted for new database structure

--- EnzymkinetikAddIn/Managers/DataGridManager.cs
+++ EnzymkinetikAddIn/Managers/DataGridManager.cs
@@ -12,38 +12,40 @@ namespace EnzymkinetikAddIn.Managers
 {
     internal class DataGridManager
     {
-        private readonly DataGridView _dataGridView;
-        private readonly ColumnManager _columnManager;
-
-        public DataGridManager(DataGridView dataGridView)
+        public List<DataGridView> GetDataGridViews(List<DataTable> tables)
         {
-            _dataGridView = dataGridView;
-            _columnManager = new ColumnManager(_dataGridView);
+            List<DataGridView> result = new List<DataGridView>();
+            foreach (DataTable table in tables)
+            {
+
+                DataGridView dataGridView = LoadTable(table);
+                result.Add(dataGridView);
+            }
+            return result;
         }
 
-        public void LoadTable(string tableName)
+        public DataGridView LoadTable(DataTable tableData)
         {
-            DataTable tableData = DatabaseHelper.LoadTable(tableName);
-            _dataGridView.Rows.Clear();
-            _dataGridView.Columns.Clear();
+            DataGridView dataGridView = new DataGridView();
+            ColumnManager  columnManager = new ColumnManager(dataGridView);
 
             List<DataColumn> commentColumns = new List<DataColumn>();
-            _columnManager.InitializeSampleColumn();
+            columnManager.InitializeSampleColumn();
             foreach (DataColumn column in tableData.Columns)
             {
                 string columnLabel = column.ColumnName;
                 if (columnLabel.Contains("Zeit"))
                 {
                     string timeunit = columnLabel.ElementAt(6) == 'm' ? "min" : "h";
-                    _columnManager.InitializeTimeColumn(timeunit);
-                    WriteToGrid(column, tableData, "time");
+                    columnManager.InitializeTimeColumn(timeunit);
+                    WriteToGrid(column, tableData, "time", dataGridView);
                 }
                 else if (columnLabel.Contains("Verdünnung"))
                 {
                     string number = columnLabel.ElementAt(2).ToString();
                     string dataGridColumnName = "dilution_" + number;
-                    _columnManager.InitializeIntCol(dataGridColumnName, "c_" + number + "\nVerdünnung");
-                    WriteToGrid(column, tableData, dataGridColumnName);
+                    columnManager.InitializeIntCol(dataGridColumnName, "c_" + number + "\nVerdünnung");
+                    WriteToGrid(column, tableData, dataGridColumnName, dataGridView);
                 }
                 else if (columnLabel.Contains("Messwert"))
                 {
@@ -53,8 +55,8 @@ namespace EnzymkinetikAddIn.Managers
                     string dataGridColumnName = num == '1' ? $"reading_1_{c}" : $"reading_2_{c}";
                     string dataGridColumnLabel = $"c_{c}\nMesswert {num}\n{sampleunit}";
 
-                    _columnManager.InitializeDoubleCol(dataGridColumnName, dataGridColumnLabel);
-                    WriteToGrid(column, tableData, dataGridColumnName);
+                    columnManager.InitializeDoubleCol(dataGridColumnName, dataGridColumnLabel);
+                    WriteToGrid(column, tableData, dataGridColumnName, dataGridView);
                 }
                 else
                 {
@@ -62,23 +64,24 @@ namespace EnzymkinetikAddIn.Managers
                 }
             }
 
-            _columnManager.InitializeBaseCol("comment", "Kommentar");
+            columnManager.InitializeBaseCol("comment", "Kommentar");
             foreach (var column in commentColumns)
             {
-                WriteToGrid(column, tableData, "comment");
+                WriteToGrid(column, tableData, "comment", dataGridView);
             }
+            return dataGridView;
         }
 
-        private void WriteToGrid(DataColumn column, DataTable tableData, string dataGridColumnName)
+        private void WriteToGrid(DataColumn column, DataTable tableData, string dataGridColumnName, DataGridView dataGridView)
         {
-            while (_dataGridView.Rows.Count <= tableData.Rows.Count)
+            while (dataGridView.Rows.Count <= tableData.Rows.Count)
             {
-                _dataGridView.Rows.Add();
+                dataGridView.Rows.Add();
             }
 
             for (int i = 0; i < tableData.Rows.Count; i++)
             {
-                _dataGridView.Rows[i].Cells[dataGridColumnName].Value = tableData.Rows[i][column];
+                dataGridView.Rows[i].Cells[dataGridColumnName].Value = tableData.Rows[i][column];
             }
         }
     }
