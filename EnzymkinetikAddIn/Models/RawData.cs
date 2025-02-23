8ac3647872da284b8d330d4f8a2a5dec9d162688
tobias <tobiaskoch93@yahoo.de>
2025-02-23 16:50:52 +0100

    added message box for debug purposes

--- EnzymkinetikAddIn/Models/RawData.cs
+++ EnzymkinetikAddIn/Models/RawData.cs
@@ -4,6 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using EnzymkinetikAddIn.Data;
 using EnzymkinetikAddIn.Interfaces;
 using EnzymkinetikAddIn.Utilities;
@@ -14,6 +15,7 @@ namespace EnzymkinetikAddIn.Models
     {
         public Dictionary<string, DataTable> CalculateResult(string entryName)
         {
+            MessageBox.Show("Model: RawData");
             return DataTransformer.TransformFromDatabase(entryName);
         }
     }
